f8226a0c037eb5da37e3528b71786d3170724710
nbs-debian <natanbarbosa50@gmail.com>
2025-10-19 11:34:05 -0300

    add: setup da ferramental de debloat do windows

---
+++ setup-performance/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Realizando 'debloat with tweaks' do windows, de apps como edge até quanto a serviços incorporados ao
+sistema como o recall, desabilitando e proporcionando maior performance e segurança ao sistema"
+
+echo "executando o Powershell como administrador, aplique o comando abaixo"
+
+irm "https://christitus.com/win" | iex
+
+
+## dev branch (deve ser usada somente em casos particulares)
+irm "https://christitus.com/windev" | iex
